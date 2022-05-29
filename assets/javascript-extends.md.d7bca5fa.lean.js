cba354d495813ce9c7434b7dd3369c87e837c06a
Zjinxing <1182999456@qq.com>
2022-05-29 11:25:53 +0000

    actions deploy

---
+++ assets/javascript-extends.md.d7bca5fa.lean.js
@@ -0,0 +1,1 @@
+import{o as n,c as s,a}from"./app.b52713d1.js";const t='{"title":"JS的继承","description":"","frontmatter":{"title":"JS的继承","date":"2018-08-04","tags":["JavaScript"]},"relativePath":"javascript-extends.md","lastUpdated":1653823536416}',p={},o=[a('',31)];p.render=function(a,t,p,c,e,u){return n(),s("div",null,o)};export{t as __pageData,p as default};
